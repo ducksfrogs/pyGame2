bf66b87888c7cdae148cf98de1fb8e41918475de
ducksfrogs <ma_yamaki@yahoo.com>
2020-12-24 14:33:42 +0900

    d

--- game/cave/cave.py
+++ game/cave/cave.py
@@ -17,22 +17,59 @@ def main():
     velocity = 0
     score = 0
     slope = randint(1,6)
+
     sysfont = pygame.font.SysFont(None, 36)
-    Ship_image = pygame.image.load('ship.png')
+    ship_image = pygame.image.load('ship.png')
     bang_image = pygame.image.load('bang.png')
 
     holes = []
-    for xpo in range(walls):
-        in_space_down = False
-        for event in pygame.event.get()
+    for xpos in range(walls):
+        holes.append(Rect(xpos*10, 100,10,400))
+        game_over = False
+
+    while True:
+        is_space_down = False
+        for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == KEYDOWN:
                 if event.key ==K_SPACE:
-                    in_space_down = True
+                    is_space_down = True
+            if not game_over:
+                score += 10
+                velocity += -3 if is_space_down else 3
+                ship_y += velocity
 
-    edge = holes[-1].copy()
-    test = edge.move(0, slope)
-    if test.top <= 0 or test.bottom >= 600:
-        slope = randint(1,6) *(-1 if slope > 0 else 1)
+                edge = holes[-1].copy()
+                test = edge.move(0, slope)
+                if test.top <= 0 or test.bottom >= 600:
+                    slope = randint(1,6) * (-1 if slope > 0 else 1)
+                    edge.inflate_ip(0,-20)
+                edge.move_ip(10, slope)
+                holes.append(edge)
+                del holes[0]
+                holes = [x.move(-10, 0) for x in holes]
+
+                if holes[0].top > ship_y or \
+                    holes[0].bottom < ship_y + 80:
+                    game_over = True
+
+            SURFACE.fill((0, 255, 0))
+            for hole in holes:
+                pygame.draw.rect(SURFACE, (0,0,0), hole)
+            SURFACE.blit(ship_image, (0, ship_y))
+            score_image = sysfont.render("score is {}".format(score), True, (0,0,255))
+            SURFACE.blit(score_image, (600, 20))
+
+            if game_over:
+                SURFACE.blit(bang_image, (0, ship_y-40))
+
+            pygame.display.update()
+            FPSCLOCK.tick(15)
+
+    if __name__ == '__main__':
+        main()
+
+
+
